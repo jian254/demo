489d3dd1899c97e0d693b01a0da130d3e50236b0
蒋健 <2847999659@qq.com>
2017-08-02 14:24:55 +0800

    添加商品控制器

---
+++ src/main/java/com/jian/controller/GoodsController.java
@@ -0,0 +1,11 @@
+package com.jian.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by jian on 17/8/2.
+ * 商品CONTROLLER
+ */
+@Controller
+public class GoodsController {
+}
